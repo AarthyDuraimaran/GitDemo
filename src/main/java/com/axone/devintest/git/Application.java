ae00f512f14f532fa23fe1ae206554a28a9f64bd
Aarthy <aarthydurai@gmail.com>
2023-02-06 21:58:23 +0000

    Jira - committing for First time

---
+++ src/main/java/com/axone/devintest/git/Application.java
@@ -0,0 +1,8 @@
+package com.axone.devintest.git;
+
+public class Application {
+
+    public static void main(String[] args) {
+        System.out.println("This is main");
+    }
+}
---
+++ src/test/java/com/axone/devintest/git/tests/App_Test.java
@@ -0,0 +1,12 @@
+package com.axone.devintest.git.tests;
+
+import org.junit.Test;
+
+public class App_Test {
+
+    @Test
+    public void Test1()
+    {
+        System.out.println("Test1");
+    }
+}
